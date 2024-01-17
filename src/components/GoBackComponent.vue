cd5203ed9ca3113179f363e7a41bbdc78d4b4333
Damian Tadla <damiantadla@gmail.com>
2024-01-17 03:13:40 +0100

    Add new Components

---
+++ src/components/GoBackComponent.vue
@@ -0,0 +1,6 @@
+<template>
+    <font-awesome-icon
+        class="fixed bottom-0 left-0 p-4 text-white text-6xl cursor-pointer"
+        :icon="['fas', 'circle-left']"
+    />
+</template>
---
+++ src/components/HeartComponent.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+
+<template>
+    <div class="flex flex-row justify-center m-auto items-center my-10">
+        <div class="w-[80px] h-[1.5px] bg-gray-400"></div>
+        <font-awesome-icon
+            class="text-red text-2xl px-5"
+            :icon="['fas', 'heart']"
+        />
+        <div class="w-[80px] h-[1.5px] bg-gray-400"></div>
+    </div>
+</template>
