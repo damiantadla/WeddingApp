bfade1034935e4a6762c5adf17cb7b823fbec556
Damian Tadla <damiantadla@gmail.com>
2024-01-19 21:53:11 +0100

    Create and style HeartComponent.vue

--- src/components/HeartComponent.vue
+++ src/components/HeartComponent.vue
@@ -1,12 +1,12 @@
 <script setup></script>
 
 <template>
-    <div class="flex flex-row justify-center m-auto items-center my-10">
-        <div class="w-[80px] h-[1.5px] bg-gray-400"></div>
+    <div class="flex flex-row justify-center m-auto items-center my-6">
+        <div class="w-[80px] h-[1.5px] bg-white"></div>
         <font-awesome-icon
             class="text-red text-2xl px-5"
             :icon="['fas', 'heart']"
         />
-        <div class="w-[80px] h-[1.5px] bg-gray-400"></div>
+        <div class="w-[80px] h-[1.5px] bg-white"></div>
     </div>
 </template>
